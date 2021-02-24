552d11c9948c063b61a099ec4ed318798bb11257
7astro7 <zaknyy@protonmail.com>
2021-02-23 22:58:39 -0800

    adds java inorder

---
+++ java/binary_tree/inorder/src/main/java/leetInorder/Solution.java
@@ -0,0 +1,50 @@
+package leetInorder;
+import java.util.List;
+import java.util.ArrayList;
+import leetInorder.TreeNode;
+
+public class Solution {
+
+    /**
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+  Input: root = [1,null,2,3]
+  Output: [1,3,2]
+
+Example 2:
+    Input: root = []
+   Output: []
+
+ Example 3:
+   Input: root = [1]
+   Output: [1]
+
+ Example 4:
+   Input: root = [1,2]
+   Output: [2,1]
+
+ Example 5:
+   Input: root = [1,null,2]
+   Output: [1,2]
+
+ Constraints:
+    The number of nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100
+    */
+
+    public List<Integer> visited;
+    public Solution() {
+        this.visited = new ArrayList<>();
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        inorderTraversal(root.left);
+        visited.add(root.val);
+        inorderTraversal(root.right);
+        return visited;
+    }
+}
---
+++ java/binary_tree/inorder/src/test/java/leetInorder/SolutionTests.java
@@ -0,0 +1,67 @@
+package leetInorder;
+
+import org.junit.Test;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+public class SolutionTests {
+
+    public static boolean traversedOK(List<Integer> observed, 
+            List<Integer> expected) {
+       for (int i = 0; i < expected.size(); i++) {
+           if (observed.get(i) - expected.get(i) != 0) {
+               return false;
+           }
+           i++;
+       }
+       return true;
+    }
+
+    public static List<Integer> getObserved(TreeNode aRoot) {
+        Solution sol = new Solution();
+        return sol.inorderTraversal(aRoot);
+    }
+
+    @Test
+    public void test_1_2_returns_2_1() {
+        TreeNode aRoot = new TreeNode(1);
+        aRoot.left = new TreeNode(2);
+        List<Integer> expected = new ArrayList<>();
+        expected.add(2); expected.add(1); 
+        assertTrue(traversedOK(getObserved(aRoot), expected));
+    }
+
+    @Test
+    public void test_1_returns_1() {
+        TreeNode aRoot = new TreeNode(1);
+        List<Integer> expected = new ArrayList<>();
+        expected.add(0, 1); 
+        assertTrue(traversedOK(getObserved(aRoot), expected));
+    }
+
+    @Test
+    public void test_1_null_2_3_returns_1_3_2() {
+        List<Integer> expected = new ArrayList<>();
+        expected.add(0, 1); expected.add(1, 3); expected.add(2, 2); 
+        List<TreeNode> nodes = new ArrayList<>();
+        for (int i = 1; i < 4; i++) {
+            nodes.add(new TreeNode(i));
+        }
+        nodes.get(0).right = nodes.get(1);
+        nodes.get(1).left = nodes.get(2);
+        assertTrue(traversedOK(getObserved(nodes.get(0)), expected));
+    }
+
+    @Test
+    public void test_1_null_2_returns_1_2() {
+        List<Integer> expected = new ArrayList<>();
+        expected.add(1); expected.add(2); 
+        TreeNode aRoot = new TreeNode(1);
+        aRoot.right = new TreeNode(2);
+        assertTrue(traversedOK(getObserved(aRoot), expected));
+    }
+
+}
